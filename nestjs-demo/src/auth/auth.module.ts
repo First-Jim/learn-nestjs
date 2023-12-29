e0aed507ff5b7f539be771a77bee9da6c170e900
Jimmy Liu <jmliu@yuansuan.cn>
2023-12-29 14:01:58 +0800

    pnpm

--- nestjs-demo/src/auth/auth.module.ts
+++ nestjs-demo/src/auth/auth.module.ts
@@ -1,10 +1,29 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from 'src/user/user.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        console.log('configService: ', configService);
+        return {
+          secret: configService.get<string>('SECRET'),
+          signOptions: {
+            expiresIn: '1d',
+          },
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
   providers: [AuthService],
   controllers: [AuthController],
 })
